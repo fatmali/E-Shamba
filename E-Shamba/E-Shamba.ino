2f1e5978ff9c08bad3e670bba882b9451c52f342
Fatma Ali <tumz92@gmail.com>
2017-02-08 22:56:29 +0300

    Added LCD functionality

--- E-Shamba/E-Shamba.ino
+++ E-Shamba/E-Shamba.ino
@@ -12,94 +12,138 @@
 #include <Sim800l.h>
 #include <SoftwareSerial.h>
 #include <DHT.h> 
+#include <LiquidCrystal.h>
+
+
+
+/////////// CONSTANTS /////////////
 
 #define DHTPIN 7
 #define DHTTYPE DHT11
+#define OWNER_TEL "+254710446379"
 
-//////////// Variables ////////////
+//////////// GLOBAL VARIABLES ////////////
 
-DHT dht(DHTPIN, DHTTYPE);
-Sim800l Sim800l;
-bool error;
-float moistureValue;
+
+LiquidCrystal lcd(12, 13, 6, 5, 4, 3);  // Initializing LCD pins
+DHT dht(DHTPIN, DHTTYPE);               // Initializing Temperature Sensor Pins
+Sim800l Sim800l;                        // Initializing SIM module
+bool error;                             // To catch errors
+float moistureValue;                    // Amount of soil moisture
 int temperature;
 int humidity;
-int sec = 0;
-int inter=200000;
+long sec = 0;
+long inter=200000;                      //InterVal in milliseconds to update data to ThingSpeak
 String alertMessage="";
-const char* ownersNumber="+254710446379";
+
 
 void setup()
 {
   
-  
+  lcd.begin(16,2);                      
   Serial.begin(9600);
   Sim800l.begin();
   dht.begin();
+  
   delay(1000);
   Serial.println("Ready");
-  error=Sim800l.delAllSms();
+  lcd.print("Setup Complete!");
+
+  error=Sim800l.delAllSms();                         // Delete all SMS to preVent errors
   if(error) Serial.println("Deleted all sms");
+  else Serial.println("No Messages to delete");
   
 }
 
 void loop()
 {
+
+
+ ////////// READ SENSOR VALUES ////////////
+
  
  
- moistureValue = analogRead(0)/100.0;
- temperature = dht.readTemperature();
- humidity = dht.readHumidity();
+ moistureValue = analogRead(0)/100.0;              // Get moisture amount from sensor
+ temperature = dht.readTemperature();              // Read temperature from sensor
+ humidity = dht.readHumidity();                    // Read humidity from sensor
+
+ 
 
 String conditions="Temp"+(String)temperature+"\t humidity"+(String)humidity+"\t Soil Moisture"+(String)moistureValue;
-
 Serial.println(conditions);
 
-if(temperature > 35.0 || temperature < 20.0){
+
+
+
+
+////////////// SMS ALERT USER /////////////////
+
+
+
+if(temperature > 35.0 || temperature < 18.0){
       alertMessage="ALERT \n The temperature is:"+(String) temperature+". \n Please regulate before damage to crops";
-      alertUser(string2char(alertMessage));
+     if(alertUser(string2char(alertMessage))){
+      lcd.clear();
+     lcd.print("Alerted User");
+     }
   }
 
 if(humidity > 50.0 || humidity < 20.0){
    alertMessage="ALERT \n The humidity is:"+(String) humidity+". \n Please regulate before damage to crops";
-     alertUser(string2char(alertMessage));
+     if(alertUser(string2char(alertMessage))){
+      lcd.clear();
+     lcd.print("Alerted User");
+     }
 }
 
 if(moistureValue > 9.0 || moistureValue < 4.0){
    alertMessage="ALERT \n The Soil Moisture content is:"+(String)moistureValue +".\n Please regulate before damage to crops";
-     alertUser(string2char(alertMessage));
+     if(alertUser(string2char(alertMessage))){
+      lcd.clear();
+     lcd.print("Alerted User");
+     }
 }
 
  ////////////////////// Cloud //////////////////////
+
+ 
+ 
   if(millis()-sec>=inter){
+    lcd.clear();
+    lcd.print("Uploading data...");
     uploadData(temperature,humidity,moistureValue);
     sec = millis();
+    lcd.clear();
+    lcd.print("Success!");
+    delay(2000);
   }
   
   
   
   //////////////////////  LCD ///////////////////////
   
-  /**
+    
+  
+
  
+  lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("temp: ");
-  lcd.print(dht.readTemperature()); 
-  lcd.print("C");
+  lcd.print(" T    ");
+  lcd.print("H   "); 
+  lcd.print(" S");
   
   lcd.setCursor(0,1);
-  lcd.print("humidity:");
-  lcd.print(dht.readHumidity());
-  lcd.print("%");
+  lcd.print(temperature);
+  lcd.print("C  ");
+  lcd.print(humidity);
+  lcd.print("%  ");
+  lcd.print(moistureValue);
 
-   **/
-  
 
- delay(120000);
  
   
  
- 
+ delay(10000);
 }
 
 
@@ -137,7 +181,7 @@ void uploadData(float temp,float humidity,float soil){
   }
 
 bool alertUser(char* message){
-  bool res=Sim800l.sendSms(ownersNumber,message);
+  bool res=Sim800l.sendSms(OWNER_TEL,message);
   return res;
   }
 
